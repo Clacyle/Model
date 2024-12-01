bfd18c7c9a269e6a04f2fd47be93a35f6537fa89
Mathéo Tripnaux-Moreau <89200698+paradoxe-tech@users.noreply.github.com>
2024-12-01 15:07:07 +0000

    implemented new assertions about dataset shapes

--- model/validate.py
+++ model/validate.py
@@ -1,10 +1,15 @@
 def validate(dataset, config):
 
-  assert config.sequence_length == config.context_length + max(config.lags_sequence)
+  err = "❌ Inconsistency of dataset shapes or hyper-parameters"
+  assert config.sequence_length == config.context_length + max(config.lags_sequence), err
   
   for batch in dataset:
-    assert batch.past_values.shape[1] == config.sequence_length
-    assert batch.future_values.shape[1] == config.prediction_length
-    assert batch.future_time_features.shape[-1] == config.num_time_features
+    assert batch.past_values.shape[1] == config.sequence_length, err
+    assert batch.future_values.shape[1] == config.prediction_length, err
+    assert batch.future_time_features.shape[-1] == config.num_time_features, err
+
+    if config.input_size != 1:
+      assert batch.past_values.shape[-1] == config.input_size, err
+      assert batch.future_values.shape[-1] == config.input_size, err
 
   print("✅ Well-formed dataset and hyper-parameters")
