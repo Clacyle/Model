fb991de32b74a06dbebb8c02d7556944d2df9ac8
Mathéo Tripnaux-Moreau <89200698+paradoxe-tech@users.noreply.github.com>
2024-12-01 10:55:04 +0000

    added model evaluation through RMSE, MAE and MAPE

---
+++ model/evaluate.py
@@ -0,0 +1,35 @@
+from model.inference import inference
+from typing import List
+from model.types import Features
+import torch
+
+def benchmark(model, dataset: List[Features]):
+  batch_index = 0
+  for prediction in inference(model, dataset):
+    batch = dataset[batch_index]
+
+    batch_size, sequence_length, input_size = batch.past_values.shape
+    batch_size, prediction_length, input_size = batch.future_values.shape
+    
+    print(f"⏳ Passed {sequence_length} cycles to model, in order to predict {prediction_length} future cycles.\n\t📊 For person n°{batch_index + 1}, prediction => {evaluate(prediction, batch)}")
+    
+    batch_index += 1
+
+
+def evaluate(prediction, batch) -> str:
+  future_values = batch["future_values"]
+  predicted_values = prediction.sequences.mean(dim=1)
+
+  future_values_flat = future_values.view(-1, future_values.shape[-1])
+  predicted_values_flat = predicted_values.view(-1, predicted_values.shape[-1])
+
+  diff = (future_values_flat - predicted_values_flat)
+
+  mape = None
+  mae = torch.mean(torch.abs(diff)).item()
+  rmse = torch.sqrt(torch.mean(diff ** 2)).item()
+  if torch.any(future_values_flat == 0):
+    mape = torch.mean(torch.abs(diff / future_values_flat)).item() * 100
+
+  return f"\n\t\tMAE: {mae}\n\t\tRMSE: {rmse}" + (f"\n\t\tMAPE: {mape}" if mape else "")
+
--- model/validate.py
+++ model/validate.py
@@ -1,7 +1,5 @@
 def validate(dataset, config):
 
-  print(config.sequence_length)
-  
   assert config.sequence_length == config.context_length + max(config.lags_sequence)
   
   for batch in dataset:
@@ -9,4 +7,4 @@ def validate(dataset, config):
     assert batch.future_values.shape[1] == config.prediction_length
     assert batch.future_time_features.shape[-1] == config.num_time_features
 
-  print("✅ Données et paramètres conformes")
+  print("✅ Well-formed dataset and hyper-parameters")
