733dc1a3557ea8cdf5e2737fbb189939632e6708
Mathéo Tripnaux-Moreau <89200698+paradoxe-tech@users.noreply.github.com>
2024-12-01 15:06:29 +0000

    added second dataset support

--- main.py
+++ main.py
@@ -6,18 +6,19 @@ from model.sampling import distribution
 from model.setup import setup
 from model.evaluate import benchmark
 
-from dataset import dataset
+from dataset import dataset as dataset1
+from dataset2 import dataset as dataset2
 
 # import config from JSON and build model
 model = setup("modelconfig.json")
 
 # build batches from dataset
-trainingSet, validationSet, testingSet = batch(dataset, model.config)
-
-# run simple tests about batch features
+trainingSet, validationSet, testingSet = batch(dataset1, model.config)
 validate(trainingSet, model.config)
-
-# train the model on the training data
 model, outputs = training(model, trainingSet)
 
-benchmark(model, validationSet)
+trainingSet, validationSet, testingSet = batch(dataset2, model.config)
+validate(trainingSet, model.config)
+model, outputs = training(model, trainingSet)
+
+benchmark(model, testingSet)
